8e02715edd5013068c37a1e37c6cbd8e711846bb
Ibrahim <ibrahim6a@yahoo.co.uk>
2020-05-08 13:02:39 +0100

    Updates

---
+++ precache-manifest.799310db68f7e0c600bba7d6af0edff2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9efabf7fe78184ff91515d7a28c0f3c",
+    "url": "/get-users/index.html"
+  },
+  {
+    "revision": "6640641cd3ab600dec75",
+    "url": "/get-users/static/js/2.ceedca9c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/get-users/static/js/2.ceedca9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c459f511fc447eb5eb10",
+    "url": "/get-users/static/js/main.9e356308.chunk.js"
+  },
+  {
+    "revision": "fb1fa61b5fa14d6f1625",
+    "url": "/get-users/static/js/runtime-main.29fddd2a.js"
+  }
+]);
